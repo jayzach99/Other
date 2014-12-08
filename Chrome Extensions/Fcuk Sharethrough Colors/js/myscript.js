bc09458cdac87818c11197a3796919aa079fcbfc
Jaison Z <jayzach99@gmail.com>
2014-12-08 03:35:55 -0500

    using style sheets instead of binding events

--- Chrome Extensions/Fcuk Sharethrough Colors/js/myscript.js
+++ Chrome Extensions/Fcuk Sharethrough Colors/js/myscript.js
@@ -1,66 +1,13 @@
-//alert("Hello");
-//var x = jQuery(".btn");
-//console.log(x);
+var css = '.btn-info:hover, .btn-info:focus, .btn-info:active, .btn-info.active, .open .dropdown-toggle.btn-info { background: rgb(0, 0, 0); background-color: rgb(0, 0, 0); border-color:rgb(0, 0, 0); } .pub-reporting-tpl .no-data-message, .pub-dash-tpl .error, .pub-brand-filter .green, .pretty-inv-code.ng-binding, .green, .green.ng-hide, .tab.ng-animate.selected-add, .value.count.ng-binding, .tab.ng-animate.clickable-add, .value.ng-binding, .error.ng-animate.ng-hide-add, .tab.clickable span, .fa.fa-arrow-circle-left.big-icon.cf-float-left.right-margin, .big.green.light, .highlight-info.ng-binding { color: rgb(242, 103, 58); } .round-workflow-icon, .text-center.day.ng-scope.ng-binding.btn-info, .btn.btn-primary.float-right, .btn.btn-primary.btn-block, .btn.btn-large.btn-primary.light-font, .btn.btn-primary.cf-float-right.ng-animate.inactive-add, .btn.btn-primary.btn-add.pull-right.top-margin, .btn btn-primary.btn-block, .round-workflow-icon, .pub-reporting-tpl .round-workflow-icon, .dropdown-menu.extended li p.green, .btn-primary, .btn-info, .modal-header, .btn-success { background: rgb(242, 103, 58); background-color: rgb(242, 103, 58); border-color:rgb(242, 103, 58); }  ';
+var head = document.head || document.getElementsByTagName('head')[0];
+var style = document.createElement('style');
 
-//var elems = document.querySelectorAll("span.ng-binding");
-
-var elems = jQuery("span.ng-binding");
-
-jQuery.each(elems, function (index, elem) {
-	console.log(elem.innerText);
-	if( elem.innerText == "Dashboard" ||  "Placement Management" || "Reporting" || "Brand Targeting"	) {
-		jQuery( elem ).bind( "click", function() {
- 			 colorize();
-		});
-		//to ensure all objects are bound ... weird thing with parents ya know
-		jQuery( jQuery(elem).parent() ).bind( "click", function() {
- 			 colorize();
-		});
-	}
-});
-
-/*
-for ( var n = 0, length = elems.length; n < length; n++ ) {
-	if ( elems[n].innerText == "Dashboard" ) {
-		jQuery(elems[n]).bind( "click", function() {
-  			colorize();
-		});
-	}
+style.type = 'text/css';
+if (style.styleSheet) {
+	style.styleSheet.cssText = css;
+} 
+else {
+	style.appendChild(document.createTextNode(css));
 }
-*/
 
-	// .. or adding custom stylesheets
-
-	var css = '.pretty-inv-code.ng-binding { color: rgb(242, 103, 58);  }',
-    head = document.head || document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
-
-	style.type = 'text/css';
-	if (style.styleSheet) {
-  		style.styleSheet.cssText = css;
-	} 
-	else {
- 	 style.appendChild(document.createTextNode(css));
-	}
-
-	head.appendChild(style);
-
-
-function colorize() {
-	var items = document.body.getElementsByTagName("*");
-
-	// Two ways to colorize ... finding all the object with color ...
-	for ( var i = 0, l = items.length; i < l; i++ ) {
-		if( window.getComputedStyle(items[i]).backgroundColor == "rgb(88, 201, 243)" || 
-			window.getComputedStyle(items[i]).backgroundColor == "rgb(65, 202, 192)" ) {
-	   		items[i].style.backgroundColor = "rgb(242, 103, 58)";
-	   		items[i].style.borderColor = "rgb(242, 103, 58)";
-
-		}
-		if( window.getComputedStyle(items[i]).color == "rgb(88, 201, 243)" ||
-			window.getComputedStyle(items[i]).color == "rgb(65, 202, 192)" ||
-			window.getComputedStyle(items[i]).color == "rgb(108, 202, 201)" ) {
-	   		items[i].style.color = "rgb(242, 103, 58)";
-	   	}
-	}
-}
+head.appendChild(style);
